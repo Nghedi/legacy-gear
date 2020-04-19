20bf2555541ea295614c9f1589168562e735ba52
Nghedi <nghedi@nghedi.de>
2020-04-19 17:21:05 +0200

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/base_layout.html")
+def base_layout():
+        return render_template("base_layout.html")
+
+@app.route("/items")
+def GetData():
+        f = open('data/resto-shaman.json', 'r')
+        daten = json.load(f)
+        columns = ["Stam", "Agi"]
+        return render_template("items.html", records=daten, colnames=columns, website="https://tbc.cavernoftime.com")
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
